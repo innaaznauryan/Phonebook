cecba5f40bff32e0c44ede899ca5a237ff140c82
innaaznauryan <inna.aznauryan@gmail.com>
2023-09-16 16:43:00 +0400

    no rights reserved

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ const App = () => {
         <h1>Phonebook</h1>
       </header>
       <List />
+      <footer>
+        <p className='bold'>&copy; Inna Aznauryan 2023</p>
+        <p className='small'>No rights reserved</p>
+      </footer>
     </>
   )
 }
